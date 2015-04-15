5e9de280619e0061948006ec5aa9f91bad9da2a9
aandeers <anol84@hotmail.com>
2015-04-15 19:35:47 +0200

    Made it possible to output csv to string instead of file.

--- src/main/scala/com/gingersoftware/csv/ObjectCSV.scala
+++ src/main/scala/com/gingersoftware/csv/ObjectCSV.scala
@@ -1,8 +1,10 @@
 package com.gingersoftware.csv
 
-import scala.reflect.runtime.{universe => ru}
-import com.github.tototoshi.csv.{CSVWriter, CSVFormat, CSVReader}
+import java.io.{FileWriter, Writer}
 
+import com.github.tototoshi.csv.{CSVReader, CSVWriter}
+
+import scala.reflect.runtime.{universe => ru}
 
 /**
  * Created by dorony on 01/05/14.
@@ -13,18 +15,23 @@ object ObjectCSV {
     val csvReader = CSVReader.open(inputPath)
     val data = csvReader.all()
     val header = data.head
-    if (!header.head.startsWith("#"))
+    if (!header.head.startsWith("#")) {
       throw new Exception("Expected a commented out header. Found: " + header)
+    }
     val headerWithoutComments = Array(header.head.substring(1)) ++ header.tail
     val objects = data.view.tail.map(row => objectConverter.toObject[T](row, headerWithoutComments))
     objects.toVector
   }
 
   def writeCSV[T <: Product : ru.TypeTag](objects: Seq[T], outputPath: String) {
+    writeCSV(objects, new FileWriter(outputPath))
+  }
+
+  def writeCSV[T <: Product : ru.TypeTag](objects: Seq[T], writer: Writer) {
     val converter = new ObjectConverter
-    val header = converter.getHeader[T]
-    val headerCapitalized = header.map(capitalizeFirstLetter(_))
-    val csvWriter = CSVWriter.open(outputPath)
+    val header = converter.getHeader[T]()
+    val headerCapitalized = header.map(capitalizeFirstLetter)
+    val csvWriter = CSVWriter.open(writer)
     csvWriter.writeRow(Seq("#" + headerCapitalized.head) ++ headerCapitalized.tail)
     val rows = objects.view.map(o => converter.fromObject(o, header))
     rows.foreach(r => csvWriter.writeRow(r))
--- src/test/scala/com/gingersoftware/csv/ObjectCSVTests.scala
+++ src/test/scala/com/gingersoftware/csv/ObjectCSVTests.scala
@@ -1,22 +1,31 @@
 package com.gingersoftware.csv
 
-import org.scalatest.FunSuite
+import java.io.{File, StringWriter}
+
 import com.gingersoftware.csv.ObjectCSV._
-import java.io.File
+import org.scalatest.FunSuite
 
 /**
  * Created by dorony on 01/05/14.
  */
 class ObjectCSVTests extends FunSuite {
-  val fileName = "test_csv.csv"
   test("can write and read csv file") {
+    val fileName = "test_csv.csv"
     val file = new File(fileName)
-    if (file.exists())
+    if (file.exists()) {
       file.delete()
+    }
     val person1 = new Person("Doron,y",10,5.5)
     val person2 = new Person("David",20,6.5)
     writeCSV(IndexedSeq(person1,person2), fileName)
     val peopleFromCSV = readCSV[Person](fileName)
     assert(peopleFromCSV === IndexedSeq(Person("Doron,y",10,5.5),Person("David",20,6.5)))
+  }
+
+  test("can write string") {
+    val person1 = new Person("Doron,y", 10, 5.5)
+    val stringWriter = new StringWriter
+    writeCSV(IndexedSeq(person1), stringWriter)
+    assert(stringWriter.toString.contains("Doron,y"))
   }
 }
